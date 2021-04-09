aa59c3a7699c4ea59a0cabadecf8f99a96a56235
0J3 <0J3@wafflepowered.com>
2021-04-09 16:41:00 +0200

    a

--- js/youtube.js
+++ js/youtube.js
@@ -2,6 +2,14 @@
 (() => {
 	const v = document.createElement('div');
 	v.classList.add('auroraBackground');
-
 	document.body.appendChild(v);
+
+	let c;
+	setInterval(() => {
+		if (!c) {
+			c = document.querySelector('#country-code');
+		}
+		c.innerText = 'AuroraWeb';
+		console.log('c');
+	}, 1000);
 })();
